97287024d562cd18b01527e6c7ef3e08775b21bd
atlasacopian <atlasacopian@gmail.com>
2025-04-15 21:34:58 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-// index.js
-
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 10000;
 
-app.use(express.json());
-
 app.get("/", (req, res) => {
-  const receipt = `<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+  const receipt = `<?xml version="1.0" encoding="UTF-8"?>
 <StarPrint>
   <text>Hello from Atlas\n</text>
   <text>Printed using Star SDK\n</text>
-  <cut type=\"partial\" />
-</StarPrint>`;
+  <cut type="partial"/>
+</StarPrint>\n`;
 
-  res.set("Content-Type", "application/xml");
+  res.set("Content-Type", "application/vnd.star.starprnt");
+  res.set("Content-Transfer-Encoding", "8bit");
   res.send(receipt);
 });
 
